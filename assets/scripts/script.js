1639a27bb39448ca4f26a6181c58913b301edc9f
Danny Calise <dannyjcalise@gmail.com>
2019-09-22 11:53:02 -0400

    got api calls working

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,19 @@
-$('.test').text("lololo");
+$('#submit-button').on('click', function() {
+    event.preventDefault();
+    $('#bike-area').css('display', 'block');
+    var cityInput = $('#city-input').val().trim();
+
+    var queryURL = 'https://api.openbrewerydb.org/breweries?by_city=' + cityInput;
+    // console.log(queryURL);
+    
+    $.get(queryURL).then(function(response) {
+        var name = response.name;
+        var city = response.city;
+        var state = response.state;
+        var phone = response.phone;
+        if (response.website_URL !== "") {
+            var url = response.website_URL;
+        }
+        // Make a card that contains all of this info
+    });
+});
