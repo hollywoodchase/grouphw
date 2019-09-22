105a43e8f555380a87c510e0ab62ccac5ef9b7aa
Danny Calise <dannyjcalise@gmail.com>
2019-09-22 18:04:56 -0400

    added cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,27 @@
 $('#submit-button').on('click', function() {
     event.preventDefault();
-    $('#bike-area').css('display', 'block');
+    $('#brewery-area').css('display', 'block');
     var cityInput = $('#city-input').val().trim();
 
     var queryURL = 'https://api.openbrewerydb.org/breweries?by_city=' + cityInput;
+    var card = {
+        
+    }
     // console.log(queryURL);
     
     $.get(queryURL).then(function(response) {
-        var name = response.name;
-        var city = response.city;
-        var state = response.state;
-        var phone = response.phone;
+
+        var randomNum = Math.floor(Math.random()*20);
+        var name = $(response)[randomNum].name;
+        var city = $(response)[randomNum].city;
+        var state = $(response)[randomNum].state;
+        var phone = $(response)[randomNum].phone;
         if (response.website_URL !== "") {
-            var url = response.website_URL;
+            var url = $(response)[randomNum].website_url;
         }
+        
         // Make a card that contains all of this info
+        $('#brewery-area').append('<div class="col-sm-5"><div class="card"><div class="card-header">' + name + '</div><div class="card-body"><h5 class="card-title">' + city + '</h5><p class="card-text">' + state + '</p><p>' + phone + '</p><a href="' + url + '" class="btn btn-primary">Visit their website</a></div></div></div>');
+        // console.log($(response));
     });
 });
